2b815ff3b70bbb6d58a421740189ebefb1a6f8e1
puruj <puru123456789@hotmail.com>
2024-11-04 13:49:45 -0500

    add dbcontext class

---
+++ DevScribe.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using DevScribe.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevScribe.API.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //represents collection of entities from a type in relational database
+        //allows to interact with entities in the database
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
