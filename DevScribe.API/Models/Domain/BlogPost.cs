9c100f194bf284cd4032acb408b63055c3834c44
puruj <puru123456789@hotmail.com>
2024-11-04 13:42:34 -0500

    added entity nugget, and made blogpost and category class

---
+++ DevScribe.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace DevScribe.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string FeaturedUImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
