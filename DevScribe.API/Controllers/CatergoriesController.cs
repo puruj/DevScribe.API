27886ed1d960779c62aca2c61a94d86bb4f9b205
puruj <puru123456789@hotmail.com>
2024-11-04 22:27:32 -0500

    Created POST action method for Categories

---
+++ DevScribe.API/Controllers/CatergoriesController.cs
@@ -0,0 +1,46 @@
+﻿using DevScribe.API.Data;
+using DevScribe.API.Models.Domain;
+using DevScribe.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevScribe.API.Controllers
+{
+    //base route to controller: https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatergoriesController : ControllerBase
+    {
+        private readonly ApplicationDBContext applicationDBContext;
+
+        public CatergoriesController(ApplicationDBContext applicationDBContext)
+        {
+            this.applicationDBContext = applicationDBContext;
+        }
+
+        //
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            //Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await applicationDBContext.Categories.AddAsync(category);
+            await applicationDBContext.SaveChangesAsync();
+
+            //Don't want to return Domain Model so must map Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+    }
+}
